330728970be608bcad4e4834b5335f94558af1c4
Stephan Kurpjuweit <stephan.kurpjuweit@thinking-apps.com>
2022-05-09 17:31:58 +0200

    Initial Commit

---
+++ memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by Stephan Kurpjuweit on 09.05.22.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
